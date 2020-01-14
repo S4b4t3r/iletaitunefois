199827ab4f91b71009f7da17c18a206d62a304fc
Théo BRD <toto.billardey@gmail.com>
2020-01-14 13:00:29 +0100

    Header

--- application/views/Header_view.php
+++ application/views/Header_view.php
@@ -9,5 +9,22 @@
 </head>
 <body>
     <header>
+        <div class="header-container">
+            <h2>Menu</h2>
+            <a href="#" class="header-button active">Accueil</a>
+            <a href="#" class="header-button">Bibliothèque en ligne</a>
+            <a href="#" class="header-button">Donations</a>
+            <a href="#" class="header-button">Notre association</a>
+            <a href="#" class="header-button">Nos activités</a>
+            <a href="#" class="header-button">Programme du mois</a>
+            <a href="#" class="header-button">Langue maternelle</a>
+            <a href="#" class="header-button">Galerie photos</a>
+            <a href="#" class="header-button">Livre d'or</a>
+            <a href="#" class="header-button">Partenaires</a>
+            <a href="#" class="header-button">Adhésion/a>
+            <a href="#" class="header-button">Contact</a>
+            <a href="#" class="header-button">Newsletter</a>
+            <a href="#" class="header-button">Règlement d'ordre intérieur</a>
 
+        </div>
     </header>
