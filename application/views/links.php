a12ae0a2f18a5a810bd3e1a48d748dbb208733ec
Théo BRD <toto.billardey@gmail.com>
2020-01-15 16:36:06 +0100

    Notre association, links

--- application/views/links.php
+++ application/views/links.php
@@ -1,7 +1,7 @@
-<a href="#" class="header-button active">Accueil</a>
+<a href="accueil" class="header-button active">Accueil</a>
             <a href="#" class="header-button">Bibliothèque en ligne</a>
-            <a href="#" class="header-button">Donations</a>
-            <a href="#" class="header-button">Notre association</a>
+            <a href="aide" class="header-button">Donations</a>
+            <a href="notreassociation" class="header-button">Notre association</a>
             <a href="#" class="header-button">Nos activités</a>
             <a href="#" class="header-button">Programme du mois</a>
             <a href="#" class="header-button">Langue maternelle</a>
---
+++ application/views/notreassociation_view.php
@@ -0,0 +1,25 @@
+<div class="content-wrapper">
+    <div class="langue">
+        <span>FR</span> | <a>EN</a>
+    </div>
+    <div class="content">
+        <h1 style="text-align: center;" class="">Notre association</h1>
+
+        <section class="aide-section">
+            <h2>À propos de nous</h2>
+            <p>
+            "Il était une fois ..." asbl est une association sans but lucratif, créée en 2004 et composée de bénévoles multilingues passionnés par la littérature, la culture et l'éducation des enfants.
+            Nous proposons des activités multilingues aux particuliers et aux écoles / crèches / foyers de jour / maisons-relais afin de permettre aux enfants de pratiquer leur langue maternelle en dehors du cercle familial et de faciliter leur apprentissage de la langue de socialisation (le luxembourgeois) et des langues étrangères.
+            En 2010, l'association a ouvert ses nouveaux locaux à Gasperich, le « Coin de Lecture », qui se compose d'une bibliothèque avec un large choix de livres en 26 langues et d’une salle dédiée aux activités. Notre défi est de changer le concept même de la bibliothèque, entendue comme un espace où règne le silence, en proposant un lieu convivial, ouvert et vivant, un lieu de rencontres et d’échanges.
+            Nous sommes soutenus dans notre travail par le CLAE, le Fonds du logement et la Ville de Luxembourg qui nous a fourni ces installations et nous aide à couvrir le loyer. Nos coûts permanents tels que les frais généraux, les frais de personnel et les stocks sont entièrement financés par les frais d'adhésion, les activités.
+            </p>
+        </section>
+
+        <section class="aide-section">
+            <h2>Nos valeurs et nos principes.</h2>
+            <p>
+                La mission de l'association est de favoriser l'identité sociale et l'intégration, avec la conviction que toutes les différentes langues et cultures présentes au Luxembourg peuvent vivre et interagir ensemble de manière positive.
+                Nous accompagnons les enfants dans leur découverte de la littérature afin d’éveiller et motiver le plaisir de lire. Nous leur permettons d’enrichir non seulement leur imaginaire mais aussi leur capacité à s’exprimer.
+                Un principe fondateur de l'association, basé sur une recherche linguistique bien établie, est que pour bien apprendre une langue étrangère, une base solide de la langue maternelle est essentielle.</p>
+        </section>
+    </div>
