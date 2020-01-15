fb47edf86e0e5b850842eb25adca63c8c5b4c901
Théo BRD <toto.billardey@gmail.com>
2020-01-15 15:19:38 +0100

    links

--- application/views/Accueil_view.php
+++ application/views/Accueil_view.php
@@ -2,20 +2,7 @@
     <!-- header responsive -->
     <div class="test" id="test">
     <h2>Menu</h2>
-            <a href="#" class="header-button active">Accueil</a>
-            <a href="#" class="header-button">Bibliothèque en ligne</a>
-            <a href="#" class="header-button">Donations</a>
-            <a href="#" class="header-button">Notre association</a>
-            <a href="#" class="header-button">Nos activités</a>
-            <a href="#" class="header-button">Programme du mois</a>
-            <a href="#" class="header-button">Langue maternelle</a>
-            <a href="#" class="header-button">Galerie photos</a>
-            <a href="#" class="header-button">Livre d'or</a>
-            <a href="#" class="header-button">Partenaires</a>
-            <a href="#" class="header-button">Adhésion</a>
-            <a href="#" class="header-button">Contact</a>
-            <a href="#" class="header-button">Newsletter</a>
-            <a href="#" class="header-button">Règlement d'ordre intérieur</a>
+        <?php require 'links.php' ?>
     </div>
     <script src="<?php echo base_url()?>assets/js/header.js"></script>
     <!-- header responsive -->
---
+++ application/views/links.php
@@ -0,0 +1,14 @@
+<a href="#" class="header-button active">Accueil</a>
+            <a href="#" class="header-button">Bibliothèque en ligne</a>
+            <a href="#" class="header-button">Donations</a>
+            <a href="#" class="header-button">Notre association</a>
+            <a href="#" class="header-button">Nos activités</a>
+            <a href="#" class="header-button">Programme du mois</a>
+            <a href="#" class="header-button">Langue maternelle</a>
+            <a href="#" class="header-button">Galerie photos</a>
+            <a href="#" class="header-button">Livre d'or</a>
+            <a href="#" class="header-button">Partenaires</a>
+            <a href="#" class="header-button">Adhésion</a>
+            <a href="#" class="header-button">Contact</a>
+            <a href="#" class="header-button">Newsletter</a>
+            <a href="#" class="header-button">Règlement d'ordre intérieur</a>
